abc475c1da8374276aceb7ceed02f9c174b7e823
liukun <liukun44919731992@gmail.com>
2016-12-02 22:59:42 +0800

    第一次

---
+++ website/mysite/src/main/java/com/phychology/mysite/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.phychology.mysite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Kevin on 2016/12/1.
+ */
+@Controller
+public class TestController {
+    @RequestMapping(value = "/aa")
+    public String test() {
+        return "aa";
+    }
+}
