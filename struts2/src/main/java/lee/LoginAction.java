fdb4a482c952f756fda1c334d5d24cce411eaf93
liukun <liukun@klsw.com>
2017-05-27 11:38:56 +0800

    修改

---
+++ struts2/src/main/java/lee/LoginAction.java
@@ -0,0 +1,15 @@
+package lee;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liukun
+ * Date: 2017/5/27
+ * Time: 11:38
+ */
+public class LoginAction implements Action {
+
+
+    public String execute() throws Exception {
+        return null;
+    }
+}
