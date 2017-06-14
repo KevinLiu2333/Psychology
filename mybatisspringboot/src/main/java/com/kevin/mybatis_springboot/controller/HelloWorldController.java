747e105e961123cd5d432536e6c176baff738d59
liukun <liukun@klsw.com>
2017-06-14 11:29:16 +0800

    修改

--- mybatisspringboot/src/main/java/com/kevin/mybatis_springboot/controller/HelloWorldController.java
+++ mybatisspringboot/src/main/java/com/kevin/mybatis_springboot/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.kevin.common.mapper.TbCwkMapper;
 import com.kevin.common.model.TbCwk;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,14 +20,15 @@ import java.util.List;
  */
 @RestController
 public class HelloWorldController {
+    private final static Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
     @Resource
     private TbCwkMapper cwkMapper;
 
     @RequestMapping("hello")
     public Object helloWorld() {
-        PageHelper.startPage(1,5);
-        List<TbCwk> list  =  cwkMapper.selectAll();
+        PageHelper.startPage(1, 5);
+        List<TbCwk> list = cwkMapper.selectAll();
         PageInfo<TbCwk> pageInfo = new PageInfo<TbCwk>(list);
         return pageInfo;
     }
