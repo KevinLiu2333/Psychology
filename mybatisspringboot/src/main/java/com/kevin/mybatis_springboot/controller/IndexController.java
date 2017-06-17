84c62e791864924cd1c368d8c9a1ce78b3295e33
liukun <liukun@klsw.com>
2017-06-17 16:44:13 +0800

    修改

---
+++ mybatisspringboot/src/main/java/com/kevin/mybatis_springboot/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.kevin.mybatis_springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liukun
+ * Date: 2017/6/17
+ * Time: 13:45
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("index")
+    public String index(Model model, HttpServletRequest request){
+        model.addAttribute("name","刘坤");
+        request.getSession().setAttribute("name","刘坤");
+        return "/index";
+    }
+}
